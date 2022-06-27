b8919b94700aaaa87d2c8be2d66e0e8fb0784f84
Hamza Zaafrane <zaafranehamza@gmail.com>
2022-06-27 12:21:18 +0100

    eventListener added

--- app/src/main/java/com/example/contact/ContactAdapter.java
+++ app/src/main/java/com/example/contact/ContactAdapter.java
@@ -1,11 +1,14 @@
 package com.example.contact;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -33,6 +36,17 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.MViewHol
     public void onBindViewHolder(@NonNull ContactAdapter.MViewHolder holder, int position) {
         Contact contact=contextList.get(position);
         holder.imgPhoto.setImageResource(contact.img);
+        holder.tvTel.setText(contact.telephone);
+        holder.tvNomPrenom.setText(contact.nom);
+        holder.imgInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mcontext, contact.telephone, Toast.LENGTH_SHORT).show();
+                Uri uri = Uri.parse("tel:56030344");
+                Intent intent=new Intent(Intent.ACTION_DIAL,uri);
+                mcontext.startActivity(intent);
+            }
+        });
 
 
     }
@@ -43,13 +57,13 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.MViewHol
     }
 
     public class MViewHolder extends RecyclerView.ViewHolder {
-        ImageView imgPhoto,imgTel,imgCall;
+        ImageView imgPhoto,imgTel,imgInfo;
         TextView tvNomPrenom,tvTel;
         public MViewHolder(@NonNull View itemView) {
             super(itemView);
             imgPhoto=itemView.findViewById(R.id.imageViewPhoto);
             imgTel=itemView.findViewById(R.id.imageViewCall);
-            imgCall=itemView.findViewById(R.id.imageViewInfo);
+            imgInfo=itemView.findViewById(R.id.imageViewInfo);
             tvNomPrenom=itemView.findViewById(R.id.textViewNom);
             tvTel=itemView.findViewById(R.id.textViewTel);
         }
